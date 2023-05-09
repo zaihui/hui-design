571c3934ddf7fb97d381c3202a893ce0dd2b5a29
xiechensheng <xiechensheng@kezaihui.com>
2023-05-09 17:00:21 +0800

    feat: 增加FilterContent

---
+++ src/components/Filter/FiltersContent/FiltersContent.tsx
@@ -0,0 +1,189 @@
+import Taro from '@tarojs/taro'
+import { observer } from 'mobx-react'
+import { View, Text } from '@tarojs/components'
+import React, { useEffect, useState, useMemo } from 'react'
+import cx from 'classnames'
+import HuiPopup, { HuiPopupProps } from '../../Popup'
+import HuiButton from '../../Button'
+import HuiIcon from '../../Icon'
+import filterStore from './store'
+
+import './FiltersContent.scss'
+
+export interface FiltersContentProps extends HuiPopupProps {
+  /** 筛选内容 */
+  contentClassName?: string
+  contentStyle?: React.CSSProperties
+  popupContentClassName?: string
+  /** 筛选模式 */
+  type?: 'single' | 'multiple'
+  /** 筛选项是否展开 */
+  filterVisible?: boolean
+  filterItems: FilterItemProps[]
+  onChange?: (val?: any) => void
+  /** 筛选 */
+  onConfirm?: (val?: any) => void
+  /** 点击遮罩层和筛选必用 */
+  onClose?: () => void
+}
+
+export interface FilterItemProps {
+  label: string
+  name: string
+  /** 筛选项的值 */
+  value?: any
+  onConfirm?: () => void
+  children?: React.ReactNode
+}
+
+const prefix = 'filters-content'
+
+const FiltersContent: React.FC<FiltersContentProps> = props => {
+  const {
+    contentClassName = '',
+    contentStyle,
+    position = 'right',
+    popupContentClassName = 'fitlers-default-popup-content',
+    type = 'single',
+    filterVisible = true,
+    filterItems = [{ label: '', name: '', value: '', children: '' }],
+    onConfirm,
+    onClose,
+    visible,
+    ...rest
+  } = props
+  const { filters, updateFilters } = filterStore
+
+  const FilterItem: React.FC<FilterItemProps> = itemProps => {
+    const { label = '筛选条件名称', name = '', value, children } = itemProps
+    const [childrenVisible, setChildrenVisible] = useState<boolean>(
+      filterVisible || false,
+    )
+
+    useEffect(() => {
+      if (!visible) return
+      if (React.isValidElement(copyChildren)) {
+        if (copyChildren.props.onChange) {
+          updateFilters && updateFilters({ [name]: value })
+
+          Taro.nextTick(() => {
+            if (
+              type === 'single'
+              && filters[name] !== null
+              && filters[name] !== undefined
+              && value !== filters[name]
+            ) {
+              onOk(name)
+            }
+          })
+        }
+      }
+    }, [value, visible])
+
+    const copyChildren = useMemo(
+      () => children && React.cloneElement(children as any),
+      [children],
+    )
+
+    return (
+      <View className={`${prefix}-item`}>
+        <View className={`${prefix}-item-title`}>
+          <Text className={`${prefix}-item-title-label`}>{label}</Text>
+          <View
+            className={`${prefix}-item-title-toggle`}
+            onClick={() => setChildrenVisible(!childrenVisible)}
+          >
+            <Text>收起</Text>
+            <HuiIcon
+              name='h011-downward'
+              className={cx({
+                upward: !childrenVisible,
+                downward: childrenVisible,
+              })}
+            />
+          </View>
+        </View>
+        <View
+          className={cx(`${prefix}-item-content`, {
+            [`${prefix}-item-content-active`]: childrenVisible,
+          })}
+        >
+          {copyChildren}
+        </View>
+      </View>
+    )
+  }
+
+  const onOk = name => {
+    if (onConfirm) {
+      onConfirm(type === 'single' ? filters[name] : filters)
+      if (onClose) onClose()
+    }
+  }
+
+  const ActionFooter = useMemo(
+    () => (
+      <View className={`action-container ${position}`}>
+        <HuiButton
+          className='filters-button'
+          type='secondary'
+          radiusType='square'
+          color='#111111'
+          style={{
+            borderColor: '#DDDDDD',
+          }}
+          onClick={() => {
+            filterStore.updateFilters({})
+          }}
+        >
+          清空筛选项
+        </HuiButton>
+        <HuiButton
+          radiusType='square'
+          color='#1569EE'
+          className='filters-button'
+          onClick={() => onOk('')}
+        >
+          筛选
+        </HuiButton>
+      </View>
+    ),
+    [filters, position],
+  )
+
+  return (
+    <View>
+      <HuiPopup
+        visible={visible}
+        position={position}
+        contentClassName={`fitlers-default-popup-content ${popupContentClassName}`}
+        onClose={() => {
+          if (onClose) {
+            filterStore.updateFilters({})
+            onClose()
+          }
+        }}
+        {...rest}
+      >
+        <View
+          className={cx(`${prefix}-content ${position} ${contentClassName}`)}
+          style={contentStyle}
+        >
+          {filterItems.length
+            && filterItems.map(item => (
+              <FilterItem
+                label={item.label}
+                name={item.name}
+                value={item.value}
+              >
+                {item.children}
+              </FilterItem>
+            ))}
+        </View>
+        {type === 'multiple' && ActionFooter}
+      </HuiPopup>
+    </View>
+  )
+}
+
+export default observer(FiltersContent)
--- src/pages/Filter/Filter.tsx
+++ src/pages/Filter/Filter.tsx
@@ -1,55 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from '@tarojs/components'
 import HuiFilter from '@/components/Filter'
 import HuiButton from '@/components/Button'
 import PageHeader from '@/demoComponents/PageHeader'
 import GroupSection from '@/demoComponents/GroupSection'
+import HuiStepper from '@/components/Stepper'
 
 import './Filter.scss'
 
 const MenuPage: React.FC = () => {
-  const options = [{ value: '1', text: '再惠' }, { value: '2', text: '麒麟' }]
+  const options = [
+    { value: '1', text: '再惠' },
+    { value: '2', text: '麒麟' },
+  ]
 
-  return <View className='filter-page'>
-    <PageHeader
-      title='Filter组件'
-      desc='筛选器'
-    />
-    <View className='content'>
-      <GroupSection title='单维度筛选（支持传入options配置或children）'>
-        <HuiFilter menuConfig={
-          {
-            className: 'hui-menu-xxx',
-            menuItems: [{
-              value: '1',
-              options,
-            }],
-          }
-        }
+  const [valState, setValState] = useState({
+    val1: 0,
+  })
+
+  const mockFiltersConfig = [
+    {
+      key: 'stepper',
+      label: '步进器',
+      name: 'stepper',
+      value: valState.val1,
+      children: (
+        <HuiStepper
+          type='number'
+          value={valState.val1}
+          onChange={val => {
+            setValState({
+              ...valState,
+              val1: val,
+            })
+          }}
         />
-      </GroupSection>
-      <GroupSection title='下拉框自定义'>
-        <HuiFilter menuConfig={
-          {
-            className: 'hui-menu-xxx',
-            menuItems: [{
-              title: '自定义title',
-              children: ({ hideMenu }) => <View>
-                <HuiButton onClick={() => hideMenu()}>ok</HuiButton>
-              </View>,
-            }, {
-              options,
-              onChange: item => {
-                console.log(item)
-              },
-              value: options[0].value,
-            }],
-          }
-        }
-        />
-      </GroupSection>
+      ),
+    },
+  ]
+
+  return (
+    <View className='filter-page'>
+      <PageHeader title='Filter组件' desc='筛选器' />
+      <View className='content'>
+        <GroupSection title='单维度筛选（支持传入options配置或children）'>
+          <HuiFilter
+            menuConfig={{
+              className: 'hui-menu-xxx',
+              menuItems: [
+                {
+                  value: '1',
+                  options,
+                },
+              ],
+            }}
+            filtersContentConfig={{
+              filterItems: mockFiltersConfig,
+            }}
+          />
+        </GroupSection>
+        <GroupSection title='下拉框自定义'>
+          <HuiFilter
+            menuConfig={{
+              className: 'hui-menu-xxx',
+              menuItems: [
+                {
+                  title: '自定义title',
+                  children: ({ hideMenu }) => (
+                    <View>
+                      <HuiButton onClick={() => hideMenu()}>ok</HuiButton>
+                    </View>
+                  ),
+                },
+                {
+                  options,
+                  onChange: item => {
+                    console.log(item)
+                  },
+                  value: options[0].value,
+                },
+              ],
+            }}
+            filtersContentConfig={{
+              filterItems: mockFiltersConfig,
+            }}
+          />
+        </GroupSection>
+      </View>
     </View>
-  </View>
+  )
 }
 
 export default MenuPage
