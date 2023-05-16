446e0066a4b0edfa282d6fdfb0090cdf9a67f0d7
haixiao yang <790041427@qq.com>
2023-05-16 17:00:51 +0800

    fix: 标题栏走查修复

--- src/pages/Title/Title.tsx
+++ src/pages/Title/Title.tsx
@@ -20,11 +20,11 @@ const TitlePage: React.FC = () => (
         <HuiTitle className='hui-demo-title' title='标题名称' size={HuiTitleSize.Large} />
       </GroupSection>
       <GroupSection title='标题&链接/交互'>
-        <HuiTitle className='hui-demo-title' title='标题名称' size={HuiTitleSize.Small} linkRightIcon='012-right' />
-        <HuiTitle className='hui-demo-title' title='标题名称名称' size={HuiTitleSize.Medium} link='全部门店' linkRightIcon='012-right' />
-        <HuiTitle className='hui-demo-title' title='标题名称' size={HuiTitleSize.Large} link='全部门店' linkRightIcon='012-right' />
-        <HuiTitle className='hui-demo-title' title='标题名称名称' size={HuiTitleSize.Medium} link='联系门店' linkLeftIcon='074-phonefill' />
-        <HuiTitle className='hui-demo-title' title='标题名称' size={HuiTitleSize.Large} link='联系门店' linkLeftIcon='074-phonefill' />
+        <HuiTitle className='hui-demo-title' title='标题名称' size={HuiTitleSize.Small} linkRightIcon='h010-right' />
+        <HuiTitle className='hui-demo-title' title='标题名称名称' size={HuiTitleSize.Medium} link='全部门店' linkRightIcon='h010-right' />
+        <HuiTitle className='hui-demo-title' title='标题名称' size={HuiTitleSize.Large} link='全部门店' linkRightIcon='h010-right' />
+        <HuiTitle className='hui-demo-title' title='标题名称名称' size={HuiTitleSize.Medium} link='联系门店' linkLeftIcon='h025-phone' />
+        <HuiTitle className='hui-demo-title' title='标题名称' size={HuiTitleSize.Large} link='联系门店' linkLeftIcon='h025-phone' />
       </GroupSection>
     </View>
   </View>
