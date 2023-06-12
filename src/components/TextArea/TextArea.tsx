bd773c5bd4fdd8202ac80c37b26c3da96942f352
xiaojindong <xiaojindong@kezaihui.com>
2023-06-12 16:23:22 +0800

    feat: 补充HuiTextArea类型

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -2,13 +2,12 @@
 import React, { useMemo, useState } from 'react'
 import cx from 'classnames'
 import { View, Textarea, Text } from '@tarojs/components'
-import { CommonEventFunction } from '@tarojs/components/types/common'
 import { TextareaProps } from '@tarojs/components/types/Textarea'
 import { HIconType } from '../Icon/type'
 import HuiIcon from '../Icon'
 
 const requiredMsg = '此为必填项'
-export interface HuiTextAreaProps {
+export interface HuiTextAreaProps extends TextareaProps {
   /** 字段名字 */
   label?: React.ReactNode
   /** 字段名字的辅助icon */
@@ -17,38 +16,12 @@ export interface HuiTextAreaProps {
   /** 报错信息 */
   errorMsg?: React.ReactNode
   upperLimit?: number
-  /** 占位提示文案 */
-  placeholder?: string
-  /** 是否禁用 */
-  disabled?: boolean
-  /** 获取焦点 */
-  focus?: boolean
-  /** 键盘弹起时，是否自动上推页面 */
-  adjustPosition?: boolean
   /** 最大输入长度，设置为 -1 的时候不限制最大长度，默认140 */
   maxLength?: number
-  /** 默认信息 */
-  value?: string
-  /** 是否显示键盘上方带有”完成“按钮那一栏 */
-  showConfirmBar?: boolean
-  /** 是否去掉 iOS 下的默认内边距 */
-  disableDefaultPadding?: boolean
   /** 必须要设置一个高度才能用 */
   height?: string | number
   style?: React.CSSProperties
   className?: string
-  /** 输入框行数变化时调用，event.detail = {height: 0, heightRpx: 0, lineCount: 0} */
-  onLineChange?: CommonEventFunction<TextareaProps.onLineChangeEventDetail>
-  /** onInput事件 */
-  onInput?: CommonEventFunction<TextareaProps.onInputEventDetail>
-  /** 输入框失获取焦点时触发 */
-  onFocus?: CommonEventFunction<TextareaProps.onFocusEventDetail>
-  /** 输入框失去焦点时触发 */
-  onBlur?: CommonEventFunction<TextareaProps.onBlurEventDetail>
-  /** 点击完成按钮时触发 */
-  onConfirm?: CommonEventFunction<TextareaProps.onConfirmEventDetail>
-  /** 键盘高度发生变化的时候触发此事件 */
-  onKeyboardHeightChange?: CommonEventFunction<TextareaProps.onKeyboardHeightChangeEventDetail>
 }
 
 const HuiTextArea: React.FC<HuiTextAreaProps> = (props) => {
@@ -58,6 +31,7 @@ const HuiTextArea: React.FC<HuiTextAreaProps> = (props) => {
     required = true,
     value,
     maxLength = 140,
+    maxlength,
     placeholder,
     disabled,
     focus,
@@ -74,6 +48,7 @@ const HuiTextArea: React.FC<HuiTextAreaProps> = (props) => {
     height,
     style,
     className = '',
+    ...rest
   } = props
   const valueLen = useMemo(() => (value && value.length) || 0, [value])
   const [errorMsg, setErrorMsg] = useState(props.errorMsg)
@@ -109,7 +84,7 @@ const HuiTextArea: React.FC<HuiTextAreaProps> = (props) => {
       placeholder={placeholder}
       disabled={disabled}
       value={value}
-      maxlength={maxLength}
+      maxlength={maxlength || maxLength}
       cursorSpacing={32}
       placeholderClass='placeholder'
       className='text-area'
@@ -131,6 +106,7 @@ const HuiTextArea: React.FC<HuiTextAreaProps> = (props) => {
       onBlur={onBlur}
       onConfirm={onConfirm}
       onKeyboardHeightChange={onKeyboardHeightChange}
+      {...rest}
     />
   )
 
