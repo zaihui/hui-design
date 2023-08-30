5167aacf62a7cad1c7177f9aefe04bc07c687a1d
wuxiuran <wuxiuran@kezaihui.com>
2023-08-30 21:35:22 +0800

    fix: Optimized writing method

--- src/components/Form/FormItem/index.tsx
+++ src/components/Form/FormItem/index.tsx
@@ -121,12 +121,12 @@ const Item: React.FC<HuiFormItemProps> = (props) => {
 
   const copyChildren = () => {
     if (React.isValidElement(children)) {
-      if (!children.props.hasOwnProperty('onChange')) {
+      if (!Object.prototype.hasOwnProperty.call(children.props, 'onChange')) {
         newProps.current.onChange = onChange
       } else {
         delete newProps.current.onChange
       }
-      if (!children.props.hasOwnProperty('value')) {
+      if (!Object.prototype.hasOwnProperty.call(children.props, 'value')) {
         newProps.current.value = localValue
       } else {
         delete newProps.current.value
